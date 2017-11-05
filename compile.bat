c4c08b0b52cda35616ccb2af339ca7ff3140f57b
Michal Pleban <michalpleban@gmail.com>
2017-11-05 23:51:16 +0100

    Fix compile.bat for compiling the ROM

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ nasm src/pc_tsr.asm -o bin/pc_tsr.com
 nasm src/pc_high.asm -o bin/pc_high.com
 nasm src/pc_high.asm -dDEBUG -o bin/pc_debug.com
 
-nasm src/rom.asm -o rom/card.bin
+nasm src/rom.asm -w-lock -o rom/card.bin
 
 nasm src/misc/reboot.asm -o bin/reboot.com
 nasm src/misc/cls.asm -o bin/cls.com
